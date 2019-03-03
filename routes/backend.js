d58335b60f04727685119608f993b54a0cfe31f4
EvaKozakova26 <evkozakova@seznam.cz>
2019-03-03 15:51:24 +0100

    implemented rest of endpoints without auth

--- routes/backend.js
+++ routes/backend.js
@@ -8,6 +8,7 @@ var ShoppingList = require('../model/shopping_list.js');
 
 // enble cors
 app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header( "Access-Control-Allow-Credentials", true);
@@ -23,19 +24,19 @@ var connectionDB = mysql.createConnection({
 connectionDB.connect();
 
 
-app.post("/api/newItem", (req, res, next) => {
-    let newItem = {
-        count: req.body.count,
-        created_at: new Date().toLocaleString(),
-        name: req.body.name,
-        state: req.body.state
-    }
 
-    Item.create({ count: newItem.count, created_at: newItem.created_at, name: newItem.name, state: newItem.state }).then(task => {
-        console.log("created item hoho " + task);
-        res.send(task);
+app.get("/api/getLists", (req, res, next) => {
+     ShoppingList.findAll().then(lists => {
+        res.send(lists)
     });
+});
 
+app.post("/api/getItems", (req, res, next) => {
+    Item.findAll({where: {
+        shopping_list_id: req.body.id
+        }}).then(items => {
+        res.send(items)
+    });
 });
 
 app.post("/api/saveList", (req, res, next) => {
@@ -59,5 +60,75 @@ app.post("/api/saveList", (req, res, next) => {
 
     })
 });
+
+
+app.post("/api/newItem", (req, res, next) => {
+    let newItem = {
+        count: req.body.count,
+        created_at: new Date().toLocaleString(),
+        name: req.body.name,
+        state: req.body.state
+    }
+
+    Item.create({ count: newItem.count, created_at: newItem.created_at, name: newItem.name, state: newItem.state }).then(task => {
+        console.log("created item hoho " + task);
+        res.send(task);
+    });
+
+});
+
+app.put("/api/checkItem", (req, res, next) => {
+
+    Item.update({
+        state: req.body.state
+    }, {
+        where: { id: req.body.id },
+    }).then()
+
+});
+
+
+app.delete("/api/deleteItem", (req, res, next) => {
+
+    Item.destroy({
+        where: {
+            id: req.body.id
+        }
+    })
+    res.send(req.body)
+
+});
+
+app.delete("/api/deleteList", (req, res, next) => {
+
+    ShoppingList.destroy({
+        where: {
+            id: req.body.id
+        }
+    })
+    res.send(req.body)
+
+});
+
+app.post("/api/updateList", (req, res, next) => {
+
+    req.body.items.forEach(function (item) {
+        Item.update({
+            shopping_list_id: req.body.shoppingList.id,
+        }, {
+            where: { id: item.id },
+        })
+            .then(function (result) {
+                console.log(result);
+            })
+    })
+    res.send(req.body.shoppingList)
+
+});
+
+
+
+
+
 
 module.exports = app;
